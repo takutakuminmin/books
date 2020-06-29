c4b2058df7bb0fb75746c7a83b7b8d0e0a79508a
takutakuminmin <takutakuminmin0730@icloud.com>
2020-06-29 16:07:38 +0900

    いらないので削除２

--- db/schema.rb
+++ db/schema.rb
@@ -12,19 +12,11 @@
 
 ActiveRecord::Schema.define(version: 2020_06_27_092346) do
 
-  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "text"
-    t.integer "user_id"
-    t.integer "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "image"
     t.text "title"
-    t.integer "page"
-    t.integer "line"
+    t.text "page"
+    t.text "line"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
