5179375c769ce40f644cb749b09607ad2e3ff4b6
takutakuminmin <takutakuminmin0730@icloud.com>
2020-06-29 17:59:57 +0900

    コメント機能実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ApplicationRecord
-
-  # has_many :comments
   validates :title, :content, presence: true
   belongs_to :user
+  has_many :comments  # commentsテーブルとのアソシエーション
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  resources :posts, except: :index
+  resources :posts do
+    resources :comments, only: :create
+  end
   resources :users, only: :show
 end
