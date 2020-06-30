4b7c36ace0b2efc39d5d78cc6a55aa56de9bafb8
takutakuminmin <takutakuminmin0730@icloud.com>
2020-06-30 20:23:23 +0900

    ページネーション機能　実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.includes(:user).order("created_at DESC")
+    @posts = Post.includes(:user).order("created_at DESC").page(params[:page]).per(10)
   end
 
   def new
