3df08c4346b94391f5fcba25cdea06494315d041
takutakuminmin <takutakuminmin0730@icloud.com>
2020-07-07 20:39:02 +0900

    コードの微調整

--- db/migrate/20200627052934_create_posts.rb
+++ db/migrate/20200627052934_create_posts.rb
@@ -1,11 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.text :title, null: false
+      t.text :title
       t.text :author
       t.text :page
       t.text :line
-      t.text :content, null: false
+      t.text :content
       t.integer :likes_count
       t.timestamps
     end
