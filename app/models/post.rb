3d713d4eee211e309735d13fe334344f26364d71
takutakuminmin <takutakuminmin0730@icloud.com>
2020-07-08 21:52:20 +0900

    ルーティング変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   validates :title, :content, presence: true
   belongs_to :user
   has_many :comments
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   def like_user(user_id)
     likes.find_by(user_id: user_id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  resources :posts do
+  resources :posts, except: :index do
     resources :likes, only: [:create, :destroy]
     resources :comments, only: :create
     collection do
