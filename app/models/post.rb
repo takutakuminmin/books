a101a915742e26d5230b49e0f8dc048d83ff7a5b
takutakuminmin <takutakuminmin0730@icloud.com>
2020-07-09 17:13:56 +0900

    submit表記変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,6 @@ class Post < ApplicationRecord
   def self.search(search)
     if search
       Post.where('title LIKE(?)', "%#{search}%")
-      # Post.where('author LIKE(?)', "%#{search}%")
     else
       Post.all
     end
