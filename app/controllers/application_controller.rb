b2ab6d9d31fee9d82755760e136f577653498a1c
takutakuminmin <takutakuminmin0730@icloud.com>
2020-08-16 16:48:51 +0900

    プロフィール機能　追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :profile])
   end
 end
