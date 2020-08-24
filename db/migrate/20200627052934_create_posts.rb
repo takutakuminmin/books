dbc4c6567b90491adf3627464632a8954f7dc6e8
takutakuminmin <takutakuminmin0730@icloud.com>
2020-08-24 13:57:43 +0900

    nulll: false　追加

--- db/migrate/20200627052934_create_posts.rb
+++ db/migrate/20200627052934_create_posts.rb
@@ -2,10 +2,10 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.text :image
-      t.text :title
+      t.text :title, null: false
       t.text :page
       t.text :line
-      t.text :content
+      t.text :content, null: false
       t.integer :likes_count
       t.timestamps
     end
